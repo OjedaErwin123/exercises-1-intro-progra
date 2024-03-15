143c47f17954eeff8132eeec6575fdd2104dc3ad
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-03-15 14:51:17 -0400

    prueba 6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -52,6 +52,18 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
+  switch (number){
+    case 1: cout <<"Hoy es LUNES";break;
+    case 2: cout <<"Hoy es MARTES";break;
+    case 3: cout <<"Hoy es MIERCOLES";break;
+    case 4: cout <<"Hoy es JUEVES";break;
+    case 5: cout <<"Hoy es VIERNES";break;
+    case 6: cout <<"Hoy es SABADO";break;
+    case 7: cout <<"Hoy es DOMINGO";break;
+
+    default : cout<< "Invalid input"
+
+  }
 }
 
 void exercise_7(double r) {
