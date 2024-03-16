39e91d8b72e5731a18bf9e5013227217410d7ca2
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-03-16 12:59:57 -0400

    commit 18

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -37,12 +37,12 @@ void exercise_8(long int seconds) {
   int x,y;
   while (seconds>3600)
   {
-    seconds=seconds-3600
+    seconds=seconds-3600;
     x++;
   }
   while (seconds>60)
   {
-    seconds=seconds-60
+    seconds=seconds-60;
     y++;
   }
   cout<<x<<":"<<y<<":"<<seconds;
