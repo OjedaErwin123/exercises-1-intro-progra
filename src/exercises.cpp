b584856078b0263be484227aa0e58bf2c8865402
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-03-16 21:55:58 -0400

    commit 79

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -107,7 +107,7 @@ void exercise_8(long int seconds) {
     y=x%60;
     cout<<setw(2)<<setfill('0')<<q<<":"
     <<setw(2)<<setfill('0')<<r<<":"
-    <<setw(2)<<setfill('0')<<y;"\n"
+    <<setw(2)<<setfill('0')<<y;"\n";
   }
   else {
     cout<<"00:00:00\n";
