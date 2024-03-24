970a87b77d6667b0711d7fe2e37812ac3fafb810
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-03-23 21:06:17 -0400

    commit 114

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -131,11 +131,7 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
 int exercise_10(int a, int b) {
   // <>TODO: YOUR CODE HERE
   int s,c,p,x;
-  
-  p=0;
- 
   c=1;
-  
   s= a+b;
   x=s;
 
