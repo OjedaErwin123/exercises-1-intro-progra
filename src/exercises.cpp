a2fcc5cebfe8426e45a43bc5d07d0c382f6003d8
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-03-25 22:24:58 -0400

    commit 143

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -177,7 +177,18 @@ string exercise_13(int age, int years_of_experience) {
 
 string exercise_14(int number_of_docs) {
   // TODO: YOUR CODE HERE
-  return "";
+  if (number_of_docs == 0){
+    return "No se encontraron documentos";
+    }
+  else if (number_of_docs == 1){
+    return "Se encontro un documento";
+  }
+  else (number_of_docs >= 2){
+    return number_of_docs<<"documentos encontrados";
+  }
+
+ 
+  
 }
 
 void exercise_15(int a, int b, int c) {
