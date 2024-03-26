64e141db4471571832b56dc0df2597f659ff9e11
OjedaErwin123 <159177988+OjedaErwin123@users.noreply.github.com>
2024-03-25 23:38:56 -0400

    commit 163

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -127,7 +127,7 @@ string exercise_9(string s1, string s2, string s3, string s4, string s5) {
   // TODO: YOUR CODE HERE
   int lon1;
   lon1=s2.length()-1;
-  if(s1[0]==s2[lon1]){
+  if(s1[0]==s5[lon1]){
     cout<<s1[0]<<s2[0]<<s3[0]<<s4[0]<<s5[0]<<"\n";
     return "Hemos encontrado algo";
   }
